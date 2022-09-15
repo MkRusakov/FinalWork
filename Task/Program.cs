7bbb6badcd899dbff9885460b471efbe6bfb6965
Mickey <mk.ru5@yandex.ru>
2022-09-15 17:42:16 +0300

    void PrintArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,6 +1,29 @@
-﻿int[] ReformArray (int count, int[] array)
+﻿void PrintArray (string[] array)
 {
-    int[] newArray = new int[array.Length];
-    
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+string[] ReformArray (int count, string[] array)
+{
+    string[] newArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length; j++)
+        {
+            string current = array[j];
+            if (current.Length <= count)
+            {
+                newArray[i] = current;
+            }
+        }
+    }
     return newArray;
 }
+string[] array = {"Hello", "Hi", "WTF"};
+PrintArray(array);
+Console.WriteLine();
+string[] newArray = ReformArray(3, array);
+PrintArray(newArray);
+
