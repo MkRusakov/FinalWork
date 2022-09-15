3856a9d45d0234022627b36120ddb0e635bec0d1
Mickey <mk.ru5@yandex.ru>
2022-09-15 18:06:54 +0300

    task finished

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,28 +2,29 @@
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]}, ");
+        Console.Write($"{array[i]} ");
     }
 }
 string[] ReformArray (int count, string[] array)
 {
     string[] newArray = new string[array.Length];
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < newArray.Length; i++)
     {
         for (int j = 0; j < array.Length; j++)
         {
             string current = array[j];
             if (current.Length <= count)
             {
-                newArray[i] = current;
+                newArray[j] = current;
             }
         }
     }
     return newArray;
 }
-string[] array = {"Hello", "Hi", "WTF"};
+string[] array = {"LOL", "Hello", "Hi", "Best", "WTF"};
 PrintArray(array);
 Console.WriteLine();
 string[] newArray = ReformArray(3, array);
 PrintArray(newArray);
+
 
